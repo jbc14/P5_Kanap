455369af8bfe829f3b4419064da2b14bd92139a8
jbc14 <jean.baptiste.conte@gmail.com>
2022-01-03 16:05:41 +0100

    Problème fetch post

--- front/js/cart.js
+++ front/js/cart.js
@@ -143,16 +143,23 @@ displayCart();
 
 //----------------------------------Validation des champs du formulaire----------------
 
+let isFirstNameValid = false;
+let isLastNameValid = false;
+let isAddressValid = false;
+let isCityValid = false;
+let isEmailValid = false;
+
 document.getElementById("firstName").addEventListener("input", function (e) {
   let firstNameFormValue = e.target.value;
   const firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
   const masqueFirstName = /[0-9]/;
 
   if (firstNameFormValue && masqueFirstName.test(firstNameFormValue)) {
-    firstNameErrorMsg.textContent = "Le champ Prénom ne doit pas contenir de chiffre";
-  }
-  else{
-    firstNameErrorMsg.textContent = " "
+    firstNameErrorMsg.textContent =
+      "Le champ Prénom ne doit pas contenir de chiffre";
+  } else {
+    firstNameErrorMsg.textContent = " ";
+    return (isFirstNameValid = true);
   }
 });
 
@@ -162,10 +169,11 @@ document.getElementById("lastName").addEventListener("input", function (e) {
   const masqueLastName = /[0-9]/;
 
   if (lastNameFormValue && masqueLastName.test(lastNameFormValue)) {
-    lastNameErrorMsg.textContent = "Le champ Nom ne doit pas contenir de chiffre";
-  }
-  else{
-    lastNameErrorMsg.textContent = " "
+    lastNameErrorMsg.textContent =
+      "Le champ Nom ne doit pas contenir de chiffre";
+  } else {
+    lastNameErrorMsg.textContent = " ";
+    return (isLastNameValid = true);
   }
 });
 
@@ -176,9 +184,21 @@ document.getElementById("city").addEventListener("input", function (e) {
 
   if (cityFormValue && masqueCity.test(cityFormValue)) {
     cityErrorMsg.textContent = "Le champ Ville ne doit pas contenir de chiffre";
+  } else {
+    cityErrorMsg.textContent = " ";
+    return (isCityValid = true);
   }
-  else{
-    cityErrorMsg.textContent = " "
+});
+
+document.getElementById("address").addEventListener("input", function (e) {
+  let addressFormValue = e.target.value;
+  const addressErrorMsg = document.getElementById("addressErrorMsg");
+
+  if (!addressFormValue) {
+    addressErrorMsg.textContent = "Le champ Adresse doit être rempli";
+  } else {
+    addressErrorMsg.textContent = " ";
+    return (isAddressValid = true);
   }
 });
 
@@ -193,8 +213,54 @@ document.getElementById("email").addEventListener("input", function (e) {
     (masqueEmail1.test(emailFormValue) && masqueEmail2.test(emailFormValue))
   ) {
     emailErrorMsg.textContent = " ";
+    return (isEmailValid = true);
   } else {
     emailErrorMsg.textContent = "Veuillez renseigner une adresse email valide";
+  }
+});
+
+//------------------------------------Commander----------------------------
+
+const orderButton = document.getElementById("order");
+class userContact {
+  constructor(firstName, lastName, address, city, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.email = email;
+  }
+}
+
+orderButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    (isFirstNameValid,
+    isLastNameValid,
+    isAddressValid,
+    isCityValid,
+    isEmailValid)
+  ) {
+    let contact = new userContact(
+      document.getElementById("firstName").value,
+      document.getElementById("lastName").value,
+      document.getElementById("address").value,
+      document.getElementById("city").value,
+      document.getElementById("email").value
+    );
+
+    fetch("http://localhost:3000/api/order", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact, getCart()),
+    });
+
+    window.location.href = "../html/confirmation.html";
+  } else {
+    alert("Merci de bien renseigner tous les champs du formulaire");
   }
 });
 
