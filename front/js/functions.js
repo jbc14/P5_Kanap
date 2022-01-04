26d061051cbcef96ca65388802fb52543a88f2fd
jbc14 <jean.baptiste.conte@gmail.com>
2022-01-04 19:12:42 +0100

    Module réussi

--- front/js/functions.js
+++ front/js/functions.js
@@ -11,36 +11,7 @@ export function getProductContent(productId) {
     });
 }
 
-export function updateCartTotal() {
-  // let cartItemsContainer = document.getElementById("cart__items");
-  // let cartItems = cartItemsContainer.getElementsByClassName("cart__item");
-  // let totalCartPrice = 0;
 
-  // for (let cartItem of cartItems) {
-  //   let cartItemDescription = cartItem.getElementsByClassName(
-  //     "cart__item__content__description"
-  //   )[0];
-  //   let cartItemPriceElement = cartItemDescription.children[2];
-  //   let cartItemPrice = parseFloat(
-  //     cartItemPriceElement.innerText.replace(" €", "")
-  //   );
-  //   let cartItemQuantity =
-  //     cartItem.getElementsByClassName("itemQuantity")[0].value;
-
-  //   let cartItemTotalPrice = cartItemPrice * cartItemQuantity;
-
-  //   totalCartPrice = totalCartPrice + cartItemTotalPrice;
-  // }
-  // document.getElementById("totalPrice").innerText = totalCartPrice;
-  const cart = getCart();
-  document.getElementById("totalPrice").innerText = cart.reduce(
-    (total, element) => {
-      const product = products.find((p) => p._id === element.id);
-      return total + element.quantity * product.price;
-    },
-    0
-  );
-}
 
 export function saveCart(cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
