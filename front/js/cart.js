d482d3c5a3bec3f2fb487ea5c8e19f71d7a013fe
jbc14 <jean.baptiste.conte@gmail.com>
2022-01-04 19:12:22 +0100

    Module réussi

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,42 @@
-import { saveCart, getCart, updateCartTotal, getProductContent } from "./functions";
+import { saveCart, getCart, getProductContent } from "./functions.js";
 
 const cartItems = document.getElementById("cart__items");
 const products = [];
+
+function displayContent(productContent, productOptions) {
+  cartItems.innerHTML += `<article class="cart__item" data-id="${productOptions.id}" data-color="${productOptions.color}">
+  <div class="cart__item__img">
+    <img src="${productContent.imageUrl}" alt="${productContent.altTxt}">
+  </div>
+  <div class="cart__item__content">
+    <div class="cart__item__content__description">
+      <h2>${productContent.name}</h2>
+      <p>${productOptions.color}</p>
+      <p>${productContent.price} €</p>
+    </div>
+    <div class="cart__item__content__settings">
+      <div class="cart__item__content__settings__quantity">
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productOptions.quantity}">
+      </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+      </div>
+    </div>
+  </div>
+</article>`;
+}
+
+function updateCartTotal() {
+  const cart = getCart();
+  document.getElementById("totalPrice").innerText = [].reduce(
+    (total, element) => {
+      const product = products.find((p) => p._id === element.id);
+      return total + element.quantity * product.price;
+    },
+    0
+  );
+}
 
 async function displayCart() {
   for (let product of getCart()) {
